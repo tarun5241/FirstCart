74a25b0237946981be0e0c64add79ef5caddc482
tarun5241 <tarunsharmapatwa@gmail.com>
2023-02-19 17:28:52 +0530

    Cart Functionalities

--- store/views.py
+++ store/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render,get_object_or_404
+from carts.models import CartItem
+from carts.views import _cart_id
 from category.models import Category
 
 from store.models import Product
@@ -27,9 +29,12 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id = _cart_id(request),product=product).exists()
+        
     except Exception as e:
         raise e
     context = {
         'product':product,
+        'in_cart':in_cart,
     }
     return render(request, 'store/product-detail.html',context)
